9db003abc5b7a98487857b358a09eb14895b23fa
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2024-11-18 15:31:29 +0100

    remove comments

--- index.js
+++ index.js
@@ -1,40 +1,29 @@
 const WebSocket = require("ws");
 
-// Maintain a set of connected clients
 const clients = new Set();
 let lastMessage = null;
 
-// Define the port
 const port = 8080;
-
-// Create WebSocket server
 const server = new WebSocket.Server({ port });
-
-// Log when the chat service starts
 console.log("Chat service started!");
 
-// Handle new client connections
 server.on("connection", (ws) => {
-  // Add the client to the set of active clients
   clients.add(ws);
   console.log(
     `New connection opened: ${ws._socket.remoteAddress}:${ws._socket.remotePort}`
   );
 
-  // Handle incoming messages from clients
   ws.on("message", (message) => {
     console.log(
       `Incoming message from ${ws._socket.remoteAddress}:${ws._socket.remotePort} => ${message}`
     );
 
-    // Save the last message
     lastMessage = {
       timestamp: Date.now(),
       client: `${ws._socket.remoteAddress}:${ws._socket.remotePort}`,
       message: message.toString(),
     };
 
-    // Broadcast the message to other clients
     clients.forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(JSON.stringify({ type: "lastMessage", data: lastMessage }));
